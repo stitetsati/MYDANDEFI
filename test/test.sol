9808f4b9cda1ee9cbf70ba19c64c46b8978a0b99
loodaobao <warren.y.cheng@gmail.com>
2023-09-03 19:05:29 +0900

    git ci

---
+++ test/test.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+import "forge-std/Test.sol";
+import "src/veLockerSrc.sol";
+
+contract TestTest is Test {
+    function testFunction() external {
+        assertTrue(true, "true is true");
+    }
+}
