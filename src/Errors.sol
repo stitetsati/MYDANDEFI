97185fc0d8d434f3eca1ce0993130a4910158478
loodaobao <warren.y.cheng@gmail.com>
2023-09-12 19:46:57 +0900

    withdraw

--- src/Errors.sol
+++ src/Errors.sol
@@ -3,3 +3,4 @@ error NotMinter();
 error InvalidArgument(uint256 argument, string message);
 error InvalidStringArgument(string argument, string message);
 error NotTokenOwner(uint256 tokenId, address messageSender, address actualOwner);
+error NotWithdrawable(uint256 tokenId, uint256 depositId, uint256 maturity);
